029152b99b861e7a79c17432fdfc58c4d3f8dafd
mysticBytes786 <snobl587@gmail.com>
2024-07-12 10:12:58 +0300

    Respond with body #cn2

--- app/main.ts
+++ app/main.ts
@@ -4,7 +4,8 @@ const server = net.createServer((socket) => {
   socket.on("data", (data: Buffer) => {
     const req: string = data.toString();
     const path = req.split(" ")[1];
-    const body = path.split("/")[2]
+    const body = path.split("/")[2];
+    console.log(body)
     
     if(path.includes("echo") || path === "/")
         return socket.write(Buffer.from(`HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${body.length}\r\n\r\n${body}`))
