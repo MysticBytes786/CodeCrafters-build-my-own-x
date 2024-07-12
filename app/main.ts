d3c5d322b7eea002bce25a85dc64e88693bea24d
mysticBytes786 <snobl587@gmail.com>
2024-07-13 01:34:07 +0300

    Read header #fs3

--- app/main.ts
+++ app/main.ts
@@ -8,15 +8,15 @@ const server = net.createServer((socket) => {
     const req: string = data.toString();
     const path = req.split(" ")[1];
     const body = path.split("/")[2];
-    const userAgent = req.split("User-Agent: ")[1].trim()
-
+    
 
     if(path.includes("echo"))
         return socket.write(Buffer.from(`${statusLine+headers+body.length}\r\n\r\n${body}`))
 
-    else if(path.includes("user-agent"))
+    else if(path.includes("user-agent")){
+        const userAgent = req.split("User-Agent: ")[1].trim()
         return socket.write(Buffer.from(`${statusLine+headers+userAgent.length}\r\n\r\n${userAgent}`))
-    
+    }
     else if(path === "/")
         return socket.write(Buffer.from('HTTP/1.1 200 OK\r\n\r\n'));
 
