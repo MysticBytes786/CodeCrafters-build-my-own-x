4dabfb67b244e4706a8bd042e788492e20d11c5f
mysticBytes786 <snobl587@gmail.com>
2024-07-12 09:34:10 +0300

    Extract URL path #ih0

--- app/main.ts
+++ app/main.ts
@@ -1,12 +1,13 @@
 import * as net from "net";
 
-// You can use print statements as follows for debugging, they'll be visible when running tests.
-console.log("Logs from your program will appear here!");
-
-// Uncomment this to pass the first stage
 const server = net.createServer((socket) => {
-  const resMsg = Buffer.from("HTTP/1.1 200 OK\r\n\r\n");
-  socket.write(resMsg);
+  socket.on("data", (data: Buffer) => {
+    const req: string = data.toString();
+    const path = req.split(" ")[1];
+    if (path !== "/")
+      socket.write(Buffer.from("HTTP/1.1 404 Not Found\r\n\r\n"));
+    else socket.write(Buffer.from("HTTP/1.1 200 OK\r\n\r\n"));
+  });
 
   socket.on("close", () => {
     socket.end();
