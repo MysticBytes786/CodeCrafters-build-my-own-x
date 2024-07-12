4293e1801312089825f87ec7a61dac0d645e9d31
mysticBytes786 <snobl587@gmail.com>
2024-07-13 01:05:46 +0300

    Read header #fs3

--- app/main.ts
+++ app/main.ts
@@ -1,16 +1,21 @@
 import * as net from "net";
 
+const statusLine = "HTTP/1.1 200 OK\r\n"
+const headers = "Content-Type: text/plain\r\nContent-Length: "
+
 const server = net.createServer((socket) => {
   socket.on("data", (data: Buffer) => {
     const req: string = data.toString();
     const path = req.split(" ")[1];
     const body = path.split("/")[2];
+    const userAgent = req.split("\n")[3].split(":")[1].trim();
+    
+
     if(path.includes("echo"))
-        return socket.write(Buffer.from(`HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${body.length}\r\n\r\n${body}`))
+        return socket.write(Buffer.from(`${statusLine + headers + body.length}\r\n\r\n${body}`))
 
     else if(path.includes("user-agent")){
-        const userAgent = req.split("\n")[3].split(":")[1];
-        return socket.write(Buffer.from(`HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${userAgent.length}\r\n\r\n${userAgent}`))
+        return socket.write(Buffer.from(`${statusLine + headers + userAgent.length}\r\n\r\n${userAgent}`))
     }
     else if(path === "/")
         return socket.write(Buffer.from('HTTP/1.1 200 OK\r\n\r\n'));
