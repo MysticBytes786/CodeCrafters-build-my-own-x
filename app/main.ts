d474b4d2c7213347712a6f4eb9161723685a5ed1
mysticBytes786 <snobl587@gmail.com>
2024-07-12 10:11:52 +0300

    Respond with body #cn2

--- app/main.ts
+++ app/main.ts
@@ -4,10 +4,10 @@ const server = net.createServer((socket) => {
   socket.on("data", (data: Buffer) => {
     const req: string = data.toString();
     const path = req.split(" ")[1];
-    const contentLength = path.split("/")[2].length
+    const body = path.split("/")[2]
     
     if(path.includes("echo") || path === "/")
-        return socket.write(Buffer.from(`HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${contentLength}\r\n\r\nabc`))
+        return socket.write(Buffer.from(`HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${body.length}\r\n\r\n${body}`))
 
     return socket.write(Buffer.from("HTTP/1.1 404 Not Found\r\n\r\n"));
   });
