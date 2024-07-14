567264f9830f6c1b9f93ae5b60242601d997409f
mysticBytes786 <snobl587@gmail.com>
2024-07-14 08:42:28 +0300

    refactor

--- app/main.ts
+++ app/main.ts
@@ -1,26 +1,120 @@
 import * as net from "net";
 
-const statusLine = "HTTP/1.1 200 OK\r\n"
-const headers = "Content-Type: text/plain\r\nContent-Length: "
+enum statusLine {
+  OK = "HTTP/1.1 200 OK\r\n",
+  NOT_FOUND = "HTTP/1.1 404 Not Found\r\n",
+}
+
+type Headers = {
+  "Content-Type"?: string;
+  "Content-Length"?: string;
+  "User-Agent"?: string;
+};
+
+type Request = {
+  status: string;
+  headers: Headers;
+  body: string;
+  method: string;
+  path: string;
+  version: string;
+};
+
+type Response = {
+  status: statusLine;
+  headers: Headers;
+  body: string;
+};
+
+enum Path {
+  root = "/",
+  echo = "/echo",
+  userAgent = "/user-agent",
+}
+
+function parseRequest(request: Buffer): Request {
+  const data = request.toString();
+  const [header, body] = data.split("\r\n\r\n");
+  const [status, ...headers] = header.split("\r\n");
+
+  const constructedHeader = headers.reduce((prevProp, header) => {
+    const [key, value] = header.split(": ");
+    return { ...prevProp, [key]: value };
+  }, {});
+  const [method, path, version] = status.split(" ");
+
+  return { status, headers: constructedHeader, body, method, path, version };
+}
+
+function constructResponse({
+  status,
+  headers = {},
+  body = "",
+}: Response): Buffer {
+  const statusBuffer = Buffer.from(status);
+  const headersBuffer = Buffer.from(
+    Object.entries(headers)
+      .map(([key, value]) => `${key}: ${value}`)
+      .join("\r\n")
+  );
+  const bodyBuffer = Buffer.from(body);
+
+  return Buffer.concat([
+    statusBuffer,
+    headersBuffer,
+    Buffer.from("\r\n\r\n"),
+    bodyBuffer,
+  ]);
+}
 
 const server = net.createServer((socket) => {
-  socket.on("data", (data: Buffer) => {
-    const req: string = data.toString();
-    const path = req.split(" ")[1];
-    const body = path.split("/")[2];
-    
+  socket.on("data", (data) => {
+    const { status, headers, body, path } = parseRequest(data);
+    const query = path.split("/").pop();
+    let response = constructResponse({
+      status: statusLine.NOT_FOUND,
+      headers: {},
+      body: "",
+    });
+    switch (path) {
+      case Path.root:
+        response = constructResponse({
+          status: statusLine.OK,
+          headers: {},
+          body: "",
+        });
+        break;
 
-    if(path.includes("echo"))
-        return socket.write(Buffer.from(`${statusLine+headers+body.length}\r\n\r\n${body}`))
+      case `${Path.echo}/${query}`:
+        const echoStr = path.split("/").pop() as string;
+        const resHeaders = {
+          "Content-Type": "text/plain",
+          "Content-Length": echoStr.length.toString(),
+        };
+        response = constructResponse({
+          status: statusLine.OK,
+          headers: resHeaders,
+          body: echoStr,
+        });
+        break;
 
-    else if(path.includes("user-agent")){
-        const userAgent = req.split("User-Agent: ")[1].trim()
-        return socket.write(Buffer.from(`${statusLine+headers+userAgent.length}\r\n\r\n${userAgent}`))
+      case Path.userAgent:
+        const hasUserAgent = "User-Agent" in headers;
+        if (hasUserAgent) {
+          const userAgent = headers["User-Agent"] ? headers["User-Agent"] : "";
+          const resHeaders = {
+            "Content-Type": "text/plain",
+            "Content-Length": userAgent?.length.toString(),
+          };
+          response = constructResponse({
+            status: statusLine.OK,
+            headers: resHeaders,
+            body: userAgent,
+          });
+        }
+        break;
     }
-    else if(path === "/")
-        return socket.write(Buffer.from('HTTP/1.1 200 OK\r\n\r\n'));
-
-    return socket.write(Buffer.from("HTTP/1.1 404 Not Found\r\n\r\n"));
+    socket.write(response);
   });
 
   socket.on("close", () => {
